be5bb876ed48dd66b2f12f584d102e33c3f8b4dc
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2020-04-20 01:41:38 -0400

    Update finalprojectschema.sql

--- finalprojectschema.sql
+++ finalprojectschema.sql
@@ -3,8 +3,8 @@ CREATE TABLE customer (
 customerid number(4),
 fname    varchar2(15) not null, 
 lname    varchar2(15) not null,
-address  varchar2(50) not null,
 phone    varchar2(10),
+address  varchar2(50) not null,
 primary key (customerid)
 );
 
@@ -13,36 +13,38 @@ CREATE TABLE driver (
 driverid number(4),
 fname    varchar2(15) not null, 
 lname    varchar2(15) not null,
-licensenum  varchar2(6) not null,
 phone    varchar2(10) not null,
+licensenum  varchar2(9) not null,
 primary key (driverid)
 );
 
 DROP TABLE restaurant CASCADE CONSTRAINTS;
 CREATE TABLE restaurant (
 restaurantid number(4),
-location   varchar2(15) not null, 
+street   varchar2(25) not null, 
+town   varchar2(20) not null, 
+state   varchar2(2) not null, 
 foodtype    varchar2(50) not null,
 name  varchar2(50) not null,
-primary key (restautantid)
+primary key (restaurantid)
 );
 
 DROP TABLE car CASCADE CONSTRAINTS;
 CREATE TABLE car (
-cardriverid number(4),
 carid number(4),
-registrationnum    varchar2(15) not null, 
+cardriverid number(4),
+plate    varchar2(6) not null, 
 type    varchar2(15) not null,
 primary key (cardriverid,carid),
 foreign key (cardriverid) references driver(driverid)
 );
 
-DROP TABLE order CASCADE CONSTRAINTS;
-CREATE TABLE order (
+DROP TABLE foodorder CASCADE CONSTRAINTS;
+CREATE TABLE foodorder (
+orderid    number(4),
 ordercustomerid number(4),
 orderrestaurantid number(4),
 orderdriverid number(4),
-orderid    number(4), 
 ordertime varchar2(5),
 items varchar2(100),
 price varchar2(10),
@@ -66,7 +68,7 @@ CREATE TABLE delivers_to (
 delivercustomerid     number(4),
 deliverdriverid     number(4),
 tip  varchar2(7),
-timeest varchar2(5) not null,
+timeest varchar2(7) not null,
 primary key (delivercustomerid, deliverdriverid),
 foreign key (delivercustomerid) references customer(customerid),
 foreign key (deliverdriverid) references driver(driverid)
